97a6b2020b1a1d2a0fb116b89622ca79f80d4f6a
John <jaricaya@gmail.com>
2024-12-27 18:27:29 +0800

    added practice

--- server/solution.py
+++ server/solution.py
@@ -377,3 +377,203 @@ api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
+
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref = 'users')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
+
+class Recipe(db.Model, SerializerMixin):
+    __tablename__ = 'recipes'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String, nullable = False)
+    minutes_to_complete = db.Column(db.Integer, nullable = False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError('Title is required')
+        return title
+    
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if not instructions or len(instructions) < 50:
+            raise ValueError("Instructions must be present and be atleast 50 characters long")
+        return instructions
+
+    @validates('minutes_to_complete')
+    def validate_minutes(self, key, minutes_to_complete):
+        if minutes_to_complete < 0:
+            raise ValueError("Minutes to complete must be non-negative")
+        return minutes_to_complete
+
+
+#!/usr/bin/env python3
+
+from flask import request, session, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from config import app, db, api
+from models import User, Recipe
+
+from app import db
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+
+        if not data:
+            return make_response({"message": "Invalid JSON. No data provided."}, 400)
+        
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        if not username or not password:
+            return make_response({"message": "Username and password are required."}, 422)
+        
+        user = User.query.filter(User.username == username).first()
+        if user:
+            return make_response({"message": "Username already taken."}, 422)
+
+        new_user = User(
+            username=username,
+            image_url=image_url,  
+            bio=bio  
+        )
+        new_user.password_hash = password  
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        
+        new_user_dict = new_user.to_dict()
+
+        return make_response(new_user_dict, 201)
+
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            user_dict = user.to_dict()
+            return make_response(
+                user_dict, 200)
+        else:
+            return {}, 401
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(
+            User.username == data['username']
+        ).first()
+        if user:
+            session['user_id'] = user.id
+            user_dict = user.to_dict()
+            return make_response(
+                user_dict, 200)
+        else:
+            return make_response(
+                {}, 401)
+
+class Logout(Resource):
+    def delete(self):
+        if session['user_id'] is None:
+            return make_response({"message": "Unauthorized"}, 401)
+        
+        else:    
+            session['user_id'] = None
+            return make_response({}, 204)
+
+class RecipeIndex(Resource):
+
+    def get(self):
+        if session['user_id'] is None:
+            return make_response(
+                {},
+                401
+            )
+        else:
+            recipes = [recipe.to_dict() for recipe in Recipe.query.all()]
+            return make_response(
+                recipes,
+                200
+            )
+        
+   
+    def post(self):
+        data = request.get_json()
+        user_id = session.get('user_id')
+
+        if not user_id:
+            return make_response({"message": "Unauthorized"}, 401)
+        try:
+            new_recipe = Recipe(
+                title=data['title'],
+                instructions=data['instructions'],
+                minutes_to_complete=data['minutes_to_complete'],
+                user_id=user_id 
+            )
+        except ValueError as e:
+            return make_response({"message": str(e)}, 422)
+
+        user = db.session.get(User, user_id)  
+        
+        if not user:
+            return make_response({"message": "User not found"}, 404) 
+
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        return make_response({
+            "title": new_recipe.title,
+            "instructions": new_recipe.instructions,
+            "minutes_to_complete": new_recipe.minutes_to_complete,
+            "user": {"id": user_id, "username": user.username}, 
+        }, 201)
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
